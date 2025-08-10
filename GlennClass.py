c347cb0d160c2df5bc7629cbf8b29fd3d9eb1de8
Glenn Jennings <gjennin334@student.glendale.edu>
2025-08-10 15:09:48 -0700

    __init__dictionary and get/set methods

--- GlennClass.py
+++ GlennClass.py
@@ -4,19 +4,49 @@ CSIS 151 - Assignment 5: Class Creation
 Name: Glenn Jennings    10281334
 Date: August 10, 2025
 
-Commit Message:   _normalize_the_title_() completed
+Commit Message:   __init__dictionary and get/set methods
 
-This commit implements a private string-massaging method called
-"_normalize_the_title_" which takes the incoming string and returns 
-a normalized string suitable to become a dictionary key.  If it
-returns "None" then the string is rejected.
+This commit tests the creation of the initial library dictionary at __init__
+and its needed internal get/set methods
 
 """
 
 class GlennClass:
 
+    # there are two principle attributes
+    #  _lib_inventory_    initialized to the library's contents
+    #  _valid_entry_      initialized to False
+    # and when accessed with a valid key, these additional
+    # attributes become defined:
+    #  _valid_entry_ is set to True
+    #  _title_
+    #  _author_
+    #  _publication_year_
+    #  _copies_available_
+    #  _copies_checked_out_
+    # I have defined these with _x_ as they are meant to be hidden.
+
     def __init__(self):
-        print('you are in the __init_ for the class')
+        self._lib_inventory_ = {}      # a dictionary
+        self._valid_entry_ = False
+        self._title_ = None
+        self._author_ = None
+        self._publication_year_ = None
+        self._copies_available_ = None
+        self._copies_checked_out_ = None
+        # 
+        # this init sequence is assumed error-free.  have to start somewhere.
+        # using the "direct assignment" method to build the dictioary.
+        #
+        nomn = self._normalize_the_title_("Henry Huggins")
+        datatuple = ("Henry Huggins", "Beverly Cleary", 1950, 2, 0)
+        self._lib_inventory_[nomn] = datatuple
+        #
+        nomn = self._normalize_the_title_("Fahrenheit 451")
+        datatuple = ("Fahrenheit 451", "Ray Bradbury", 1953, 0, 1 )
+        self._lib_inventory_[nomn] = datatuple
+        print('initial dictionary is built:')
+        print( self._lib_inventory_ )
 
 # internal method to massage an arbitrary string into a string
 # suitable as a dictionary key:
@@ -36,13 +66,9 @@ class GlennClass:
             return None
         # now clean up that string.  we might still get an empty string.
         stage_one = in_name.strip()             # lead and trail alank removal
-        print(f"'{stage_one}' is OK so far")
         stage_two = stage_one.replace('\t',' ') # tabs to spaces
-        print(f"'{stage_two}' is OK so far")
         stage_three = " ".join(stage_two.split()) # combine multiple spaces
-        print(f"'{stage_three}' is OK so far")
         stage_four = stage_three.lower()          # downshift all alphas
-        print(f"'{stage_four}' is OK so far")
         if stage_four == '':                    # eliminate empty strings
             print(f" ** '{in_name}' is invalid")
             return None
--- glenn_test.py
+++ glenn_test.py
@@ -4,14 +4,15 @@ CSIS 151 - Assignment 5: Class Creation
 Name: Glenn Jennings    10281334
 Date: August 10, 2025
 
-Commit Message:  _normalize_the_title_() completed
+Commit Message:  __init__dictionary and get/set methods
 
 This is file "glenn_test.py" and contains my class test code.
 here's my github repository, it is publicly readable:
     https://github.com/gjennin334/assignment-5.git
 
-This commit tests a private string-massaging method called
-"_normalize_the_title_"
+This commit tests the creation of the initial library dictionary at __init__
+and its needed internal get/set methods
+
 
 """
 
@@ -22,15 +23,8 @@ from GlennClass import *     # but this does.
 
 def main():
     new_glennclass = GlennClass()
+    # initialize the dictionary and other internal attributes:
     print("it's alive, it's alive")
-
-    new_glennclass._normalize_the_title_("good")
-    # new_glennclass._normalize_the_title_(None)
-    # new_glennclass._normalize_the_title_(["a","b"])
-    new_glennclass._normalize_the_title_(" Pre\tbonk ")
-    new_glennclass._normalize_the_title_("  ")
-    new_glennclass._normalize_the_title_(" 14 31 ")
-    new_glennclass._normalize_the_title_("duo  Duo")
     
 if __name__ == "__main__":
     main()
