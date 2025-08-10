6b84cf142b06f749d520c1a07978f2702c975a10
Glenn Jennings <gjennin334@student.glendale.edu>
2025-08-10 15:08:09 -0700

    _normalize_the_title_() completed

--- GlennClass.py
+++ GlennClass.py
@@ -2,24 +2,14 @@
 Instructor: Edwin D. Sookiassian
 CSIS 151 - Assignment 5: Class Creation
 Name: Glenn Jennings    10281334
-Date: August 8, 2025
+Date: August 10, 2025
 
-Commit Message:  @@@
+Commit Message:   _normalize_the_title_() completed
 
-This is file "GlennClass.py" and contains the class code.
-It is meant to be IMPORTED by the second file "glenn_test.py"
-where both files are in the same directory and the "current
-directory" of the CLI is also in that same directory:
-
-    # glenn_test.py
- 
-    import GlennClass
-
-    def main():
-        #  TBD @@@
-
-    if __name__ == "__main__": #DO NOT MODIFY THIS LINE
-        main() #DO NOT MODIFY THIS LINE
+This commit implements a private string-massaging method called
+"_normalize_the_title_" which takes the incoming string and returns 
+a normalized string suitable to become a dictionary key.  If it
+returns "None" then the string is rejected.
 
 """
 
@@ -28,6 +18,40 @@ class GlennClass:
     def __init__(self):
         print('you are in the __init_ for the class')
 
-# this works:  __init__ is invoked
-# m = GlennClass()
-# print(m)
+# internal method to massage an arbitrary string into a string
+# suitable as a dictionary key:
+# it will produce a lowercase string, cleaned-up regarding spaces
+# that is: no lead or trail spaces, and only single spaces between "words"
+# return "None" if input string is unacceptable or invalid
+
+    def _normalize_the_title_( self, in_name ):
+        isgood = False
+        try:
+            if isinstance( in_name, str ):   # is it a simple string ?
+                isgood = True
+        except Exception as e:
+            pass
+        if not isgood:
+            print(f" ** {in_name} is invalid")
+            return None
+        # now clean up that string.  we might still get an empty string.
+        stage_one = in_name.strip()             # lead and trail alank removal
+        print(f"'{stage_one}' is OK so far")
+        stage_two = stage_one.replace('\t',' ') # tabs to spaces
+        print(f"'{stage_two}' is OK so far")
+        stage_three = " ".join(stage_two.split()) # combine multiple spaces
+        print(f"'{stage_three}' is OK so far")
+        stage_four = stage_three.lower()          # downshift all alphas
+        print(f"'{stage_four}' is OK so far")
+        if stage_four == '':                    # eliminate empty strings
+            print(f" ** '{in_name}' is invalid")
+            return None
+        if not any(kar.isalpha() for kar in stage_four):  # at least one alpha
+            # stage_four is an iterable, a list of characters.
+            print(f" ** '{in_name}' is invalid")
+            return None
+        return stage_four
+
+
+
+            
--- glenn_test.py
+++ glenn_test.py
@@ -2,27 +2,16 @@
 Instructor: Edwin D. Sookiassian
 CSIS 151 - Assignment 5: Class Creation
 Name: Glenn Jennings    10281334
-Date: August 8, 2025
+Date: August 10, 2025
 
-Commit Message:  initial commit, my class and test skeletons
+Commit Message:  _normalize_the_title_() completed
 
 This is file "glenn_test.py" and contains my class test code.
-It is meant to IMPORTED the second file "GlennTest.py"
-where both files are in the same directory and the "current
-directory" of the CLI is also in that same directory.
-
-the result of executing "python glenn_test.py" with the files in this
-first commit is:
-
-    % python glenn_test.py
-    you are in the __init_ for the class
-    it's alive, it's alive
-
 here's my github repository, it is publicly readable:
-
     https://github.com/gjennin334/assignment-5.git
 
-it is cloned into VSCode and the first commit will be from there.
+This commit tests a private string-massaging method called
+"_normalize_the_title_"
 
 """
 
@@ -34,6 +23,14 @@ from GlennClass import *     # but this does.
 def main():
     new_glennclass = GlennClass()
     print("it's alive, it's alive")
+
+    new_glennclass._normalize_the_title_("good")
+    # new_glennclass._normalize_the_title_(None)
+    # new_glennclass._normalize_the_title_(["a","b"])
+    new_glennclass._normalize_the_title_(" Pre\tbonk ")
+    new_glennclass._normalize_the_title_("  ")
+    new_glennclass._normalize_the_title_(" 14 31 ")
+    new_glennclass._normalize_the_title_("duo  Duo")
     
 if __name__ == "__main__":
     main()
